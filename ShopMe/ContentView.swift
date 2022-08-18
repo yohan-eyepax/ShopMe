614d5d945c3eaeed3d0c2fb44151ace8dfb3d990
Yohan Alahakoon <yohanalahakoon@EYEPAXMM-07.local>
2022-08-18 13:16:33 +0530

    initial commit

---
+++ ShopMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShopMe
+//
+//  Created by Yohan Alahakoon on 2022-08-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShopMe/ShopMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopMeApp.swift
+//  ShopMe
+//
+//  Created by Yohan Alahakoon on 2022-08-18.
+//
+
+import SwiftUI
+
+@main
+struct ShopMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
